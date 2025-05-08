be45ecc8dc781aec1f9fb72def9bcd28711a54ee
Muhammad Rehan <rehan@sixfeetup.com>
2025-05-08 20:48:21 +0500

    Add initial commit on project creation Remove docker-compose dependency Make Docker python image dynamic based on runtime and architecture Update project structure to match scaf-leaderboard repo

--- tasks.py
+++ tasks.py
@@ -1,23 +1,56 @@
 import os
-import random
 import shlex
 import shutil
-import string
 import subprocess
 
 
+TERMINATOR = "\x1b[0m"
+WARNING = "\x1b[1;33m [WARNING]: "
+INFO = "\x1b[1;33m [INFO]: "
+HINT = "\x1b[3;33m"
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
+
 def run_setup():
+    print("Performing initial commit.")
+    subprocess.run(shlex.split("git add ."))
+    subprocess.run(shlex.split("git commit -m 'Initial commit' --quiet"))
+
     if not shutil.which("sam"):
         print("Error: AWS SAM CLI is not installed. Please install it and try again.")
         exit(1)
-    
+
     print("Running AWS SAM build and validate...")
-    subprocess.run(shlex.split("sam validate --lint"))
-    subprocess.run(shlex.split("sam build"))
+    subprocess.run(shlex.split("make validate"))
+    subprocess.run(shlex.split("make build"))
     print("AWS Lambda template setup complete.")
 
 
+def init_git_repo():
+    print(INFO + "Initializing git repository..." + TERMINATOR)
+    print(INFO + f"Current working directory: {os.getcwd()}" + TERMINATOR)
+    subprocess.run(shlex.split("git -c init.defaultBranch=main init . --quiet"))
+    print(SUCCESS + "Git repository initialized." + TERMINATOR)
+
+
+def configure_git_remote():
+    repo_url = "{{ copier__repo_url }}"
+    if repo_url:
+        print(INFO + f"repo_url: {repo_url}" + TERMINATOR)
+        command = f"git remote add origin {repo_url}"
+        subprocess.run(shlex.split(command))
+        print(SUCCESS + f"Remote origin={repo_url} added." + TERMINATOR)
+    else:
+        print(
+            WARNING
+            + "No repo_url provided. Skipping git remote configuration."
+            + TERMINATOR
+        )
+
+
 def main():
+    init_git_repo()
+    configure_git_remote()
     run_setup()
 
 
